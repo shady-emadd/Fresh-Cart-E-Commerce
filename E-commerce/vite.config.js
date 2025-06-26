58df3c8b7360c95e68f0858a9c9ba5d1d079fbaf
shady-emadd <shadyemad2012@live.com>
2025-06-26 09:17:20 +0300

    Update vite.config.js

--- E-commerce/vite.config.js
+++ E-commerce/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import autoprefixer from "autoprefixer";
-import postcss from "postcss";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), autoprefixer(), postcss()],
+  plugins: [react(), tailwindcss()],
   content: ["./index.html"],
 });
